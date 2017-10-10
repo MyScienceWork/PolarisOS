e245865082fe5b578d2720639fb7731fb2664468
Corentin Ribeyre <cocophotos@orange.fr>
2017-10-10 13:38:40 +0200

    Integration CSS/HTML

--- front/frontend/pages/home/Home.vue
+++ front/frontend/pages/home/Home.vue
@@ -1,15 +1,122 @@
 <template>
-<div class="container is-fluid">
-    <div class="card is-paddingless">
-        <header class="card-header card-header-red ">
-            10 Octobre - 11 Octobre
-        </header>
-        <div class="card-content">
-            Je suis en train de tester le contenu
-            <input type="text" class="input" placeholder="Ok I'm a placeholer" />
+<div class="hero is-fullheight">
+    <div class="hero-head">
+        <div class="container is-fluid">
+            <div class="tile is-ancestor">
+                <div class="tile is-parent is-2 is-vertical is-hidden-touch">
+                    <div class="image tile is-child">
+                        <img src='/public/front/imgs/logo/logo.svg' alt='Logo' title='App Logo' />
+                    </div>
+                </div>
+                <div class="tile is-vertical">
+                </div>
+                <div class="tile is-parent is-2 is-vertical">
+                    <p class="tile is-child">
+                        <a href='#'>Log In / Sign In</a>
+                    </p>
+                    <p class="tile is-child">
+                        <a href='#'>FR</a>&nbsp;|&nbsp;<a href='#'>EN</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <nav role="navigation" aria-label="main navigation">
+            <div class="container is-fluid">
+                <div class="columns is-centered">
+                    <div class="column navbar-item-red navbar-item">
+                        <a class="" href='#'>Home</a>
+                    </div>
+                    <div class="column navbar-item-orange navbar-item">
+                        <a class="" href='#'>Home</a>
+                    </div>
+                    <div class="column navbar-item-purple navbar-item">
+                        <a class="" href='#'>Home</a>
+                    </div>
+                    <div class="column navbar-item-brown navbar-item">
+                        <a class="" href='#'>Home</a>
+                    </div>
+                    <div class="column navbar-item-green navbar-item">
+                        <a class="" href='#'>Home</a>
+                    </div>
+                    <div class="column navbar-item-blue navbar-item">
+                        <a class="" href='#'>Home</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="hero-body">
+        <div class="container is-fluid">
+            Je suis le body 
         </div>
     </div>
+    <div class="hero-foot">
+        <footer>
+            <div class="container is-fluid">
+                <div class="columns footer-menu">
+                    <div class="column footer-menu-red">
+                        <p class="footer-menu-title">L'institut</p>
+                        <ul class="footer-menu-list">
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                        </ul>
+                    </div>
+                    <div class="column footer-menu-orange">
+                        <p class="footer-menu-title">Le dépôt</p>
+                        <ul class="footer-menu-list">
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                        </ul>
+                    </div>
+                    <div class="column footer-menu-purple">
+                        <p class="footer-menu-title">La consultation</p>
+                        <ul class="footer-menu-list">
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                        </ul>
+                    </div>
+                    <div class="column footer-menu-brown">
+                        <p class="footer-menu-title">L'institut</p>
+                        <ul class="footer-menu-list">
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                        </ul>
+                    </div>
+                    <div class="column footer-menu-green">
+                        <p class="footer-menu-title">L'institut</p>
+                        <ul class="footer-menu-list">
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                        </ul>
+                    </div>
+                    <div class="column footer-menu-blue">
+                        <p class="footer-menu-title">L'institut</p>
+                        <ul class="footer-menu-list">
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                            <li><a href='#'>Test</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="columns footer-disclaimer">
+                    <p class="column">&copy; INED 2017</p>
+                </div>
+            </div>
+        </footer>
+    </div>
 </div>
+
 </template>
 
 <script>
