599400a63d4956f9a9375604bd7e93c8bd73d613
Corentin Ribeyre <cocophotos@orange.fr>
2017-10-10 09:52:15 +0200

    Integration CSS/HTML

--- front/frontend/pages/home/Home.vue
+++ front/frontend/pages/home/Home.vue
@@ -1,7 +1,14 @@
 <template>
-<div>
-    <loader />
-    <p>{{state.value}}</p>
+<div class="container is-fluid">
+    <div class="card is-paddingless">
+        <header class="card-header card-header-red ">
+            10 Octobre - 11 Octobre
+        </header>
+        <div class="card-content">
+            Je suis en train de tester le contenu
+            <input type="text" class="input" placeholder="Ok I'm a placeholer" />
+        </div>
+    </div>
 </div>
 </template>
 
