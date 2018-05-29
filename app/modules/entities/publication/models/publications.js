79d624c8ff80e7f0a6450c67a1d5d1b21c77fc19
Corentin Ribeyre <cocophotos@orange.fr>
2018-05-29 09:57:21 +0200

    Nowhere default value

--- app/modules/entities/publication/models/publications.js
+++ app/modules/entities/publication/models/publications.js
@@ -263,6 +263,7 @@ const Completion: Array<any> = [
         status: (o, p, i) => ComplFunctions.generic_complete('pending')(o, p, i),
         'dates.deposit': () => ({ dates: { deposit: +moment() } }),
         'diffusion.rights.embargo': object => ({ diffusion: { rights: { embargo: Utils.find_value_with_path(object, 'dates.publication'.split('.')) || +moment() } } }),
+        'diffusion.rights.exports.nowhere': object => ({ diffusion: { rights: { exports: { nowhere: false } } } }),
     },
     {
         sherpa: async (object, path) => {
