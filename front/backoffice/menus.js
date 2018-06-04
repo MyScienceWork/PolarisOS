fedf5c100d70873c52f26bba0c30257796e14cee
Corentin Ribeyre <cocophotos@orange.fr>
2018-06-03 20:58:57 -0600

    Fix user/role access

--- front/backoffice/menus.js
+++ front/backoffice/menus.js
@@ -38,7 +38,7 @@ module.exports.menu = [
         {
             section: 'l_general_section',
             name: 'l_users_page',
-            access: '',
+            access: 'user',
             subaccess: ['c', 'r', 'u', 'd'],
             key: 'user',
             routes: [Routes.user],
@@ -48,7 +48,7 @@ module.exports.menu = [
         {
             section: 'l_general_section',
             name: 'l_roles_page',
-            access: '',
+            access: 'role',
             subaccess: ['c', 'r', 'u', 'd'],
             key: 'role',
             routes: [Routes.role],
@@ -118,7 +118,7 @@ module.exports.menu = [
             submenu: [],
             component: Query,
         },
-        {
+        /* {
             section: 'l_admin_section',
             name: 'l_mails_page',
             access: 'mail_template',
@@ -127,7 +127,7 @@ module.exports.menu = [
             routes: [Routes.mail_template],
             submenu: [],
             component: MailTemplate,
-        },
+        },*/
         {
             section: 'l_admin_section',
             name: 'l_charts_page',
