aed2c322643f5614522e442891c91571d1616a1f
louismarie <12646487+louismarie@users.noreply.github.com>
2019-01-22 09:35:30 +0100

    add loader

--- front/frontend/pages/deposit_project/DepositProject.vue
+++ front/frontend/pages/deposit_project/DepositProject.vue
@@ -5,12 +5,17 @@
             <div class="column">
                 <div class="card">
                     <div class="card-content">
-                        <dynamic-form :form="user_forms('project_form')" :cform="state.sinks.creations.project"/>
-                        <button
-                                @click.prevent="save('project')"
-                                class="button button-background-green has-small-top-margin has-small-bottom-margin">
-                            {{lang('f_save_project')}}
-                        </button>
+                        <div v-if="Object.keys(user_forms('project_form')).length === 0" class="columns is-centered">
+                            <loader />
+                        </div>
+                        <div v-else>
+                            <dynamic-form  :form="user_forms('project_form')" :cform="state.sinks.creations.project"/>
+                            <button
+                                    @click.prevent="save('project')"
+                                    class="button button-background-green has-small-top-margin has-small-bottom-margin">
+                                {{lang('f_save_project')}}
+                            </button>
+                        </div>
                         <article class="message is-success" v-if="state.statuses.creations.project === 'ok'">
                             <div class="message-body">
                                 {{lang('f_project_save_success')}}
