0e30d8febe33ada0f6e7f5a0062763f29aae4d33
louismarie <12646487+louismarie@users.noreply.github.com>
2019-11-06 18:10:38 +0100

    add security analitycs dashboard

--- front/frontend/pages/analitycs_dashboard/AnalitycsDashboard.js
+++ front/frontend/pages/analitycs_dashboard/AnalitycsDashboard.js
@@ -19,7 +19,16 @@ module.exports = {
     },
     computed: {
         route_dashboard() {
-            return `https://analitycs-${window.location.host}/app/kibana#/dashboard/8c85a970-df6d-11e9-a2c6-99b64336e7cf?embed=true`;
+            const env = process.env.NODE_ENV || 'development';
+            if (env === 'development') {
+                return 'http://localhost:5601/app/kibana#/dashboard/8c85a970-df6d-11e9-a2c6-99b64336e7cf?embed=true&_g=()&_a=(fullScreenMode:true)';
+            }
+            return 'https://analitycs-${window.location.host}/app/kibana#/dashboard/8c85a970-df6d-11e9-a2c6-99b64336e7cf?_g=()&_a=(description:\'\',filters:!(),fullScreenMode:!t,options:(darkTheme:!f,hidePanelTitles:!t,useMargins:!f),panels:!((embeddableConfig:(),gridData:(h:21,i:\'1\',w:48,x:0,y:0),id:\'0835eb20-fa80-11e9-9880-1da0dc2eca8c\',panelIndex:\'1\',type:visualization,version:\'6.8.4\')),query:(language:lucene,query:\'\'),timeRestore:!f,title:\'Dashboard%20list\',viewMode:view)';
+        },
+        heightIFrame() {
+            // 174px height header
+            // 24px height footer
+            return `${window.innerHeight - 174 - 24}px`;
         },
     },
     beforeMount() {
--- front/frontend/pages/analitycs_dashboard/AnalitycsDashboard.vue
+++ front/frontend/pages/analitycs_dashboard/AnalitycsDashboard.vue
@@ -1,5 +1,7 @@
 <template>
-<iframe :src="route_dashboard" height="1500px" width="100%"></iframe>
+    <div style="width: 90%; margin: auto">
+        <iframe id="analytics-dashboard" :src="route_dashboard" height="1000px" width="100%"></iframe>
+    </div>
 </template>
 
 <script>
