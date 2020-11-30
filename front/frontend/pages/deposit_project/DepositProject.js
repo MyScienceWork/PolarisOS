1e26e0559f52a0dc336b128e77fe4a33766ff8e9
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2020-11-30 18:06:50 +0100

    feat!: add project getter (wip)

--- front/frontend/pages/deposit_project/DepositProject.js
+++ front/frontend/pages/deposit_project/DepositProject.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const LangMixin = require('../../../common/mixins/LangMixin');
 const FormMixin = require('../../../common/mixins/FormMixin');
 const FormCleanerMixin = require('../../../common/mixins/FormCleanerMixin');
@@ -17,12 +18,10 @@ module.exports = {
                 sinks: {
                     creations: {
                         project: 'project_creation',
-                        project_type: 'project_creation_type',
                     },
                     reads: {
                         user_forms: 'user_forms_read',
                         project: 'project_read',
-                        project_type: 'project_type_read',
                     },
                 },
                 paths: {
@@ -32,7 +31,6 @@ module.exports = {
                     reads: {
                         user_forms: APIRoutes.entity('form', 'POST', true),
                         project: APIRoutes.entity('project', 'POST', true),
-                        project_type: APIRoutes.entity('project_type', 'POST', true),
                     },
                 },
                 statuses: {
@@ -42,15 +40,10 @@ module.exports = {
                 },
                 show_review_modal: false,
                 project_form_name: 'project_back',
-                project_subform_name: '',
-                project_type: '',
             },
         };
     },
     methods: {
-        workflow_name() {
-            return this.$route.query.workflow;
-        },
         is_editing() {
             return this.$route.query && this.$route.query._id;
         },
@@ -75,51 +68,18 @@ module.exports = {
         },
         project_submitted() {
             // init project type form choices
-            this.$set(this.state, 'project_type', '');
-            this.$set(this.state, 'project_subform_name', '');
             this.$store.commit(Messages.TRANSFERT_INTO_FORM, {
-                form: this.state.sinks.creations.project_type,
+                form: this.state.sinks.creations.project,
                 body: undefined,
             });
         },
-        project_type_change(form) {
-            if (!form || !form.value || form.value === '') {
-                if (this.state.project_subform_name) {
-                    this.state.project_subform_name = '';
-                    return;
-                }
-            }
-            /*
-            this.$store.commit(Messages.INITIALIZE, {
-                form: this.state.sinks.reads.user_forms,
-                keep_content: false,
-            });
-            */
-            const forms = this.fcontent(this.state.sinks.reads.project_type);
-            if (forms instanceof Array) {
-                form.name = forms.find(my_form => my_form._id === form.value).value;
-            } else {
-                return;
-            }
-            this.state.project_subform_name = `${this.state.project_form_name}_${form.name.toLowerCase()}`;
-            this.$store.dispatch('search', {
-                form: this.state.sinks.reads.user_forms,
-                path: this.state.paths.reads.user_forms,
-                body: {
-                    where: {
-                        name: [this.state.project_subform_name],
-                    },
-                    population: ['fields.subform', 'fields.datasource'],
-                },
-            });
-            /*
-            this.$store.commit(Messages.COMPLETE_FORM_ELEMENT, {
-                form: this.state.sinks.creations.project,
-                name: 'type',
-                info: form,
-            });
-            */
-            this.$set(this.state, 'project_type', form.value);
+        get_project_from() {
+            const forms = this.fcontent(this.state.sinks.reads.user_forms);
+            console.log(forms);
+            const project_back_form = _.find(forms,
+                function (o) { return o._id === this.state.project_form_name; },
+            );
+            return project_back_form;
         },
     },
     components: {
@@ -132,16 +92,6 @@ module.exports = {
                 return this.$route.query._id;
             }
             return '';
-        },
-        project_type() {
-            return this.state.project_type;
-        },
-        project_type_options() {
-            const content = this.fcontent(this.state.sinks.reads.project_type);
-            if (!(content instanceof Array)) {
-                return [];
-            }
-            return content;
         },
         depositor_user_id() {
             const content = this.fcontent(this.state.sinks.reads.project_type);
@@ -171,13 +121,6 @@ module.exports = {
         }
     },
     mounted() {
-        this.$store.dispatch('search', {
-            form: this.state.sinks.reads.project_type,
-            path: this.state.paths.reads.project_type,
-            body: {
-                size: 10000,
-            },
-        });
         this.$store.dispatch('search', {
             form: this.state.sinks.reads.user_forms,
             path: this.state.paths.reads.user_forms,
