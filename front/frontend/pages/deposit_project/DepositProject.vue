728f554fc73af03d364a92d47d3c42769441ace0
Virgil Ribeyre <virgil.ribeyre@protonmail.com>
2020-11-30 18:03:54 +0100

    feat: add project deposit (wip)

--- front/frontend/pages/deposit_project/DepositProject.vue
+++ front/frontend/pages/deposit_project/DepositProject.vue
@@ -7,24 +7,7 @@
                     <div class="card-content">
                         <h4 class="has-small-top-margin title is-4">{{lang('l_deposit_project')}}</h4>
                         <div>
-                            <fselect
-                                    :label="lang('f_choose_project_type')"
-                                    :is-required="true"
-                                    :options="project_type_options"
-                                    :form="state.sinks.creations.project_type"
-                                    name="type"
-                                    fieldLabel="label"
-                                    fieldValue="_id"
-                                    :view-validation-texts="false"
-                                    :translatable="true"
-                                    @select-change="project_type_change"
-                            />
-                            <div v-if="state.project_subform_name !== '' && Object.keys(user_forms(state.project_subform_name)).length === 0" class="column">
-                                <div class="columns is-centered">
-                                    <loader />
-                                </div>
-                            </div>
-                            <div v-else-if="state.project_subform_name !== ''">
+                            <div>
                                 <fform
                                         :name="state.sinks.creations.project"
                                         :hasButtons="!is_editing()"
@@ -35,43 +18,7 @@
                                         :get_form="state.sinks.reads.project"
                                         @form-success-reset="project_submitted"
                                 >
-                                <dynamic-form :form="user_forms(state.project_subform_name)" :cform="state.sinks.creations.project"/>
-                                    <finput
-                                        type='hidden'
-                                        label=''
-                                        :hiddenValue="project_type"
-                                        name="type"
-                                        :form="state.sinks.creations.project"
-                                    />
-                                    <finput
-                                        type="hidden"
-                                        label=''
-                                        :hiddenValue="creation_date"
-                                        name="creation_date"
-                                        :form="state.sinks.creations.project"
-                                    />
-                                    <finput
-                                        type='hidden'
-                                        label=''
-                                        :hiddenValue="project_id"
-                                        name="_id"
-                                        :form="state.sinks.creations.project"
-                                    />
-                                    <finput
-                                        type='hidden'
-                                        label=''
-                                        :hiddenValue="depositor_user_id"
-                                        name="depositor"
-                                        :form="state.sinks.creations.project"
-                                    />
-                                    <finput
-                                        v-if="!is_editing()"
-                                        type='hidden'
-                                        label=''
-                                        :hiddenValue="initial_state"
-                                        name="state"
-                                        :form="state.sinks.creations.project"
-                                    />
+                                    <dynamic-form :form="get_project_form()" :cform="state.sinks.creations.project"/>
                                     <button v-if="is_editing()"
                                             @click.prevent="open_review_modal(props)"
                                             :disabled="success"
