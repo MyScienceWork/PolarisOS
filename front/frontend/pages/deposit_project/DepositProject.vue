c0c83170219973147f7a2bf3d95fbac43320eb67
louismarie <12646487+louismarie@users.noreply.github.com>
2020-12-07 11:05:53 +0100

    reformat deposit prpject.vue

--- front/frontend/pages/deposit_project/DepositProject.vue
+++ front/frontend/pages/deposit_project/DepositProject.vue
@@ -13,50 +13,33 @@
                                     :name="state.sinks.creations.project"
                                     :hasButtons="!is_editing()"
                                     :hasCancelButton="false"
-                                    :post_path="
-                                        state.paths.creations.project
-                                    "
-                                    :put_path="
-                                        state.paths.creations.project
-                                    "
+                                    :post_path="state.paths.creations.project"
+                                    :put_path="state.paths.creations.project"
                                     :get_path="state.paths.reads.project"
                                     :get_form="state.sinks.reads.project"
                                     @form-success-reset="project_submitted"
                                 >
-                                    <dynamic-form
-                                        :form="
-                                            project_form
-                                        "
-                                        :cform="
-                                            state.sinks.creations.project
-                                        "
-                                    />
+                                    <dynamic-form :form="project_form"
+                                        :cform="state.sinks.creations.project"/>
                                     <button
                                         v-if="is_editing()"
-                                        @click.prevent="
-                                            open_review_modal(props)
-                                        "
+                                        @click.prevent="open_review_modal(props)"
                                         :disabled="success"
-                                        class="button"
-                                    >
+                                        class="button">
                                         {{ lang("f_finish_review") }}
                                     </button>
                                 </fform>
                             </div>
                             <article
                                 class="message is-success"
-                                v-if="state.statuses.creations.project === 'ok'"
-                            >
+                                v-if="state.statuses.creations.project === 'ok'">
                                 <div class="message-body">
                                     {{ lang("f_project_save_success") }}
                                 </div>
                             </article>
                             <article
                                 class="message is-danger"
-                                v-if="
-                                    state.statuses.creations.project === 'nok'
-                                "
-                            >
+                                v-if="state.statuses.creations.project === 'nok'">
                                 <div class="message-body">
                                     {{ lang("f_project_save_failed") }}
                                 </div>
