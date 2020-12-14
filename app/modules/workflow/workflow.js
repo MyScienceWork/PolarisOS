3ef035b3ad4e4dc678e56060ef3fd8e8df3f8bbc
louismarie <12646487+louismarie@users.noreply.github.com>
2020-12-14 23:39:22 +0100

    clean

--- app/modules/workflow/workflow.js
+++ app/modules/workflow/workflow.js
@@ -38,13 +38,10 @@ class Workflow {
     }
 
     static async _ok_condition(condition: string, item: Object): boolean {
-        console.log("process transition condition : ", condition);
-        console.log("process transition item : ", item);
         const validator = new Validator();
         const joi_condition = ConditionValidator.compute_condition({ field: condition });
         const errorsValidation = await validator
             .validate(item, [joi_condition]);
-        console.log("errorsValidation : ", errorsValidation);
         if (!_.isEmpty(errorsValidation)) {
             return false;
         }
@@ -57,7 +54,7 @@ class Workflow {
         }
 
         actions.forEach(action => {
-           console.log("action");
+           console.log("action : ", action);
         });
     }
 
@@ -66,9 +63,7 @@ class Workflow {
             return;
         }
         for (const condition of step.conditions) {
-            console.log("condition : ", condition);
             const action_condition_validated = await Workflow._ok_condition(condition.condition, item);
-            console.log("action_condition_validated : ", action_condition_validated);
             if (action_condition_validated) {
                 Workflow._run_action(condition.actions);
             }
@@ -82,7 +77,6 @@ class Workflow {
 
         workflows.forEach(workflow => {
             workflow.steps.forEach(step => {
-                console.log("step : ", step);
                 const run_transition_step = (
                     step.type === "transition"
                     && step.state_before.findIndex(state => state._id === state_before) !== -1
@@ -92,18 +86,11 @@ class Workflow {
                     step.type === "state"
                     && step.state_after.findIndex(state => state._id === state_after) !== -1
                 );
-
-                console.log("run_transition_step : ", run_transition_step);
-                console.log("run_state_step : ", run_state_step);
-
                 if (run_transition_step || run_state_step) {
                     Workflow._run_transition(step, item);
                 }
-
             });
         });
-        console.log("state_before : ", JSON.stringify(state_before));
-        console.log("state_after : ", JSON.stringify(state_after));
     }
 }
 
