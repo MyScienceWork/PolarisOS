ec773d74b638db61aa8af340456e96dff687602e
louismarie <12646487+louismarie@users.noreply.github.com>
2021-02-18 16:06:28 +0100

    feat: view page

--- front/frontend/pages/view/View.vue
+++ front/frontend/pages/view/View.vue
@@ -8,11 +8,21 @@
                         <a href='#' class="card-header-icon card-header-tag is-left">
                             <span class="tag is-purple is-active">{{lang(content_item.denormalization.group.label)}}</span>
                         </a>
-                        <div class="card-header-title">
-                        </div>
                     </div>
                     <div class="card-content content">
                         <h3 class="title is-3">{{content_item.title}}</h3>
+                        <h4 class="title is-4">{{_oa_find(content_item, 'denormalization.publishing_institution.name')}}</h4>
+                        <p v-if="working_paper" v-html="working_paper"></p>
+                      <p v-html="contributors.contributors"></p>
+                      <ol>
+                          <li v-for="affiliation in affiliations" v-html="affiliation"></li>
+                        </ol>
+                        <p v-if="content_item.doi">
+                          <strong>DOI</strong> : <a class="break-word has-text-purple" target='_blank' :href='`https://doi.org/${content_item.doi}`'><span class="icon is-small"><i class="fa fa-external-link"></i></span>{{content_item.doi}}</a>
+                        </p>
+                        <p v-if="content_item.url">
+                          <a class="break-word has-text-purple" target='_blank' :href='`${content_item.url}`'><span class="icon is-small"><i class="fa fa-external-link"></i></span>{{content_item.url}}</a>
+                        </p>
                         <div>
                             <widget :collapsed="true">
                                 <span slot="title">{{lang('f_publication_information')}}</span>
